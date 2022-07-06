e270702e4ff781698ccb73523ee395522346687c
Jesse Hernandez <jessehernandez@Jesses-Home-MBPro.local>
2022-07-06 14:43:18 -0700

    Module 13 Challenge Deliverable 1

---
+++ Earthquake_Challenge/static/js/logicStep1.js
@@ -0,0 +1,84 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+// We create the tile layer that will be the background of our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    // id: 'mapbox/streets-v11',
+    // id: 'mapbox/light-v10',
+    // id: 'mapbox/dark-v10',
+    id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+// streets.addTo(map);
+
+// We create the tile layer DARK background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    // id: 'mapbox/dark-v10',
+    // id: 'mapbox/satellite-streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+// Then we add our 'graymap' tile layer to the map.
+// dark.addTo(map);
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+    "Streets": streets,
+    "Satellite": satelliteStreets
+};
+
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [39.5, -98.5],
+    zoom: 3,
+    layers: [streets]
+});
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+
+// Accessing the airport GeoJSON URL
+// let airportData = "https://raw.githubusercontent.com/jhernandez1270/Mapping_Earthquakes/main/majorAirports.json";
+
+// Accessing the Toronto airline routes GeoJSON URL.
+// let torontoData = "https://raw.githubusercontent.com/jhernandez1270/Mapping_Earthquakes/main/torontoRoutes.json";
+
+// Accessing the Toronto neighborhoods GeoJSON URL.
+// let torontoHoods = "https://raw.githubusercontent.com/jhernandez1270/Mapping_Earthquakes/Mapping_GeoJSON_Polygons/torontoNeighborhoods.json";
+
+
+
+// Grabbing our GeoJSON data.
+// d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+//     console.log(data);
+//     // Creating a GeoJSON layer with the retrieved data.
+//     L.geoJSON(data, {
+//         color: "blue",
+//         weight: 1,
+//         fillColor: "yellow",
+//         fillOpacity: 0.5,
+//         onEachFeature: function (feature, layer) {
+//             console.log(layer);
+//             layer.bindPopup("<h3>Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+//         }
+
+//     }).addTo(map);
+// });
+
+
+// Grabbing our GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function (data) {
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data).addTo(map);
+});
